13e80e2a0fe03509645332597a259cd76032ba44
Simon Van den Eynde <r1003875@student.thomasmore.be>
2025-10-22 18:39:42 +0200

    fix: reorder mongoose and cors imports in index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const config = require('config');
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 /*const mongoURI = config.get('db.uri')|| process.env.MONGODB_URI;*/
 mongoose.connect("mongodb://localhost:27017/chips")
 .then(() => console.log("✅ MongoDB connected"))
 .catch(err => console.error("❌ MongoDB connection error:", err));
 
-const mongoose = require('mongoose');
-const cors = require('cors');
 const app = express();
 const port = 3000;
 
