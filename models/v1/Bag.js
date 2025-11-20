11af355eb31082df664528651f3a683f2ab288cc
Simon Van den Eynde <r1003875@student.thomasmore.be>
2025-11-20 17:02:47 +0100

    feat: add Bag, User, and Vote models with schema definitions

---
+++ models/v1/Bag.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const BagSchema = new Schema({
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    color: { type: String, required: true },
+    font: { type: String, required: true },
+    patern:{ type: String, required: true },
+    packaging: { type: String, required: true },
+    inspiration: { type: String, required: true },
+    keyFlavours: { type: [String], required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+});
+const Bag = mongoose.model('Bag', BagSchema);
+module.exports = Bag;
