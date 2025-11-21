973a8354c7213658ca9c0b574fddc05707deb389
Simon Van den Eynde <r1003875@student.thomasmore.be>
2025-11-21 18:18:56 +0100

    feat: update MongoDB connection to use config for URI

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-/*const mongoURI = config.get('db.uri')|| process.env.MONGODB_URI;*/
-mongoose.connect("mongodb://localhost:27017/chips")
+const mongoURI = config.get('db.uri')|| process.env.MONGODB_URI;
+mongoose.connect(mongoURI)
 .then(() => console.log("✅ MongoDB connected"))
 .catch(err => console.error("❌ MongoDB connection error:", err));
 
